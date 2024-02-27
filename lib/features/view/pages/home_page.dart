a1477c9e40ee49a48ca4beb5ea52da83038190bb
Sireen Ibnu Kabeer <82222070+Sireen-Ibnu-Kabeer@users.noreply.github.com>
2024-02-27 14:57:13 +0530

    minor update on valudation

--- lib/features/view/pages/home_page.dart
+++ lib/features/view/pages/home_page.dart
@@ -15,79 +15,85 @@ class HomePage extends ConsumerWidget {
   final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Scaffold(
-      appBar: const HeaderWidget(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            PhoneNumberField(
-              number: number,
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return "Phone number is empty";
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 18),
-            ElevatedButton(
-              onPressed: () async {
-                // final url =
-                //     "whatsapp://send?phone=${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
-                if (formKey.currentState!.validate()) {
-                  final chatUrl =
-                      "https://wa.me/${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
-                  try {
-                    launchUrl(
-                      Uri.parse(chatUrl),
-                      mode: LaunchMode.externalApplication,
-                    );
-                  } catch (e) {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Could not launch WhatsApp"),
-                        ),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        appBar: const HeaderWidget(),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Form(
+                key: formKey,
+                child: PhoneNumberField(
+                  number: number,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Phone number is empty";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+              const SizedBox(height: 18),
+              ElevatedButton(
+                onPressed: () async {
+                  // final url =
+                  //     "whatsapp://send?phone=${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
+                  if (formKey.currentState!.validate()) {
+                    final chatUrl =
+                        "https://wa.me/${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
+                    try {
+                      launchUrl(
+                        Uri.parse(chatUrl),
+                        mode: LaunchMode.externalApplication,
                       );
+                    } catch (e) {
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Could not launch WhatsApp"),
+                          ),
+                        );
+                      }
                     }
                   }
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 48),
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 48),
+                ),
+                child: const Text("Open in WhatsApp App"),
               ),
-              child: const Text("Open in WhatsApp App"),
-            ),
-            const SizedBox(height: 14),
-            ElevatedButton(
-              onPressed: () {
-                if (formKey.currentState!.validate()) {
-                  final url =
-                      "https://web.whatsapp.com/send?phone=${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
-                  try {
-                    launchUrl(
-                      Uri.parse(url),
-                      mode: LaunchMode.inAppBrowserView,
-                    );
-                  } catch (e) {
-                    if (context.mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Could not launch WhatsApp"),
-                        ),
+              const SizedBox(height: 14),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    final url =
+                        "https://web.whatsapp.com/send?phone=${ref.watch(selectedCountryCode).replaceAll("+", '')}${number.text}";
+                    try {
+                      launchUrl(
+                        Uri.parse(url),
+                        mode: LaunchMode.inAppBrowserView,
                       );
+                    } catch (e) {
+                      if (context.mounted) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("Could not launch WhatsApp"),
+                          ),
+                        );
+                      }
                     }
                   }
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size(double.infinity, 48),
+                },
+                style: ElevatedButton.styleFrom(
+                  minimumSize: const Size(double.infinity, 48),
+                ),
+                child: const Text("Open in WhatsApp Web"),
               ),
-              child: const Text("Open in WhatsApp Web"),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
